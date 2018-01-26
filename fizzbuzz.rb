e18355ec0c3a53ac1dfc17a3ae88176ca39dd611
Wilson Blaikie <wilsonblaikie@gmail.com>
2018-01-26 13:41:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(input)
-  if (input % 3 == 0 && input & 5 != 0)
+  if (input % 3 == 0 && input % 5 != 0)
     return "Fizz"
-  elsif (input % 3 != 0 && input & 5 == 0)
+  elsif (input % 3 != 0 && input % 5 == 0)
     return "Buzz"
-  elsif (input % 3 == 0 && input & 5 == 0)
+  elsif (input % 3 == 0 && input % 5 == 0)
     return "FizzBuzz"
   else
     return nil
